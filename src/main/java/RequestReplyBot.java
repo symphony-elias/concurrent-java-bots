037a26aac8bf945b83168f346312eacc69ceec39
Elias Croze <elias.croze@symphony.com>
2020-12-15 19:51:20 +0100

    Test CP paths in config

--- src/main/java/RequestReplyBot.java
+++ src/main/java/RequestReplyBot.java
@@ -1,4 +1,10 @@
 import clients.SymBotClient;
+import listeners.FirehoseListener;
+import model.InboundMessage;
+import model.OutboundMessage;
+import model.Stream;
+import model.User;
+import model.events.*;
 import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,20 +14,86 @@ public class RequestReplyBot {
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-
-        new Thread(() -> runBot("config.json")).start();
-        new Thread(() -> runBot("config_bis.json")).start();
+        runOneBot();
     }
 
-    private static void runBot(String configFilePath) {
+    private static void runOneBot() {
         try {
-            SymBotClient botClient = SymBotClient.initBotRsa(configFilePath);
-            botClient.getDatafeedEventsService().addListeners(
-                    new IMListenerImpl(botClient),
-                    new RoomListenerImpl(botClient)
-            );
+            SymBotClient botClient = SymBotClient.initBot("config.json");
+            botClient.getMessagesClient().sendMessage("gXFV8vN37dNqjojYS_y2wX___o2KxfmUdA", new OutboundMessage("Hello!"));
+//            botClient.getFirehoseService().addListener(new FirehoseListener() {
+//                @Override
+//                public void onRoomMessage(InboundMessage inboundMessage) {
+//                    log.debug("Message received: {}", inboundMessage.getMessage());
+//                }
+//
+//                @Override
+//                public void onRoomCreated(RoomCreated roomCreated) {
+//
+//                }
+//
+//                @Override
+//                public void onRoomDeactivated(RoomDeactivated roomDeactivated) {
+//
+//                }
+//
+//                @Override
+//                public void onRoomMemberDemotedFromOwner(RoomMemberDemotedFromOwner roomMemberDemotedFromOwner) {
+//
+//                }
+//
+//                @Override
+//                public void onRoomMemberPromotedToOwner(RoomMemberPromotedToOwner roomMemberPromotedToOwner) {
+//
+//                }
+//
+//                @Override
+//                public void onRoomReactivated(Stream stream) {
+//
+//                }
+//
+//                @Override
+//                public void onRoomUpdated(RoomUpdated roomUpdated) {
+//
+//                }
+//
+//                @Override
+//                public void onUserJoinedRoom(UserJoinedRoom userJoinedRoom) {
+//
+//                }
+//
+//                @Override
+//                public void onUserLeftRoom(UserLeftRoom userLeftRoom) {
+//
+//                }
+//
+//                @Override
+//                public void onIMMessage(InboundMessage inboundMessage) {
+//
+//                }
+//
+//                @Override
+//                public void onIMCreated(Stream stream) {
+//
+//                }
+//
+//                @Override
+//                public void onConnectionAccepted(User user) {
+//
+//                }
+//
+//                @Override
+//                public void onConnectionRequested(User user) {
+//
+//                }
+//            });
+//            botClient.getDatafeedEventsService().addListeners(
+//                    new IMListenerImpl(botClient),
+//                    new RoomListenerImpl(botClient)
+//            );
         } catch (Exception e) {
             log.error("Error", e);
         }
     }
+
 }
